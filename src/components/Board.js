e0caaf90c1da4f5fe2b9af5c8840bf31d24eb5a2
Randy West <randywest55@gmail.com>
2022-05-31 01:22:25 -0700

    Add mockup display to Board

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,3 +1,137 @@
+import pasture from "../images/pasture.png";
+import forest from "../images/forest.png";
+import hills from "../images/hills.png";
+import mountains from "../images/mountains.png";
+import fields from "../images/fields.png";
+import desert from "../images/desert.png";
+import two from "../images/2.png";
+import three from "../images/3.png";
+import four from "../images/4.png";
+import five from "../images/5.png";
+import six from "../images/6.png";
+import eight from "../images/8.png";
+import nine from "../images/9.png";
+import ten from "../images/10.png";
+import eleven from "../images/11.png";
+import twelve from "../images/12.png";
+
+// TODO: convert this back into typescript
+
+// TODO: obviously this comes in as props. probably incoming props have a
+// simpler coordinate system that lends itself to arranging them equitably, and
+// we translate somewhere here
+const hexes = [
+  { gridArea: "1 / 3 / 4 / 5", type: "fields", number: 6 },
+  { gridArea: "1 / 7 / 4 / 9", type: "mountains", number: 2 },
+  { gridArea: "1 / 5 / 4 / 7", type: "forest", number: 3 },
+  { gridArea: "3 / 2 / 6 / 4", type: "fields", number: 12 },
+  { gridArea: "3 / 4 / 6 / 6", type: "forest", number: 11 },
+  { gridArea: "3 / 6 / 6 / 8", type: "hills", number: 8 },
+  { gridArea: "3 / 8 / 6 / 10", type: "pasture", number: 4 },
+  { gridArea: "5 / 1 / 8 / 3", type: "pasture", number: 10 },
+  { gridArea: "5 / 3 / 8 / 5", type: "pasture", number: 4 },
+  { gridArea: "5 / 5 / 8 / 7", type: "desert", number: null },
+  { gridArea: "5 / 7 / 8 / 9", type: "forest", number: 11 },
+  { gridArea: "5 / 9 / 8 / 11", type: "hills", number: 3 },
+  { gridArea: "7 / 2 / 10 / 4", type: "mountains", number: 8 },
+  { gridArea: "7 / 4 / 10 / 6", type: "mountains", number: 5 },
+  { gridArea: "7 / 6 / 10 / 8", type: "forest", number: 9 },
+  { gridArea: "7 / 8 / 10 / 10", type: "fields", number: 10 },
+  { gridArea: "9 / 3 / 12 / 5", type: "fields", number: 5 },
+  { gridArea: "9 / 7 / 12 / 9", type: "hills", number: 9 },
+  { gridArea: "9 / 5 / 12 / 7", type: "pasture", number: 6 },
+];
+
+const hexNameToImg = {
+  pasture: pasture,
+  forest: forest,
+  hills: hills,
+  mountains: mountains,
+  fields: fields,
+  desert: desert,
+};
+
+const numberValToImg = [
+  null,
+  null,
+  two,
+  three,
+  four,
+  five,
+  six,
+  null,
+  eight,
+  nine,
+  ten,
+  eleven,
+  twelve,
+];
+
+/**
+ * `Board` is the display logic for a Catan board, it's props is an array of hexes,
+ * each with a coordinate, type, and number chit value
+ */
 export default function Board() {
-  return <></>;
+  // if we split each hex into a top triangle, a middle rectangle, and a bottom
+  // triangle, this weird number is the ratio of of the height of one of the
+  // triangles to that of the rectangle. more or less. it's also the number that
+  // looks right
+  const smallRowSize = 35 / 77;
+
+  return (
+    <div
+      style={{
+        margin: 20,
+        width: 800,
+        height: 750,
+        justifyItems: "center",
+        alignItems: "center",
+        display: "grid",
+        // TODO: I'm ignoring border pieces for now. they significantly
+        // complicate things
+        gridTemplateColumns: "repeat(10, 1fr)",
+        gridTemplateRows: `${smallRowSize}fr 1fr `
+          .repeat(5)
+          .concat(`${smallRowSize}fr`),
+      }}
+    >
+      {hexes.map(({ gridArea, type, number }, i) => (
+        <div
+          key={i}
+          style={{
+            height: "100%",
+            width: "100%",
+            position: "relative",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            gridArea: gridArea,
+          }}
+        >
+          <img
+            src={hexNameToImg[type]}
+            alt={type}
+            style={{
+              width: "100%",
+              height: "100%",
+              position: "absolute",
+              zIndex: 1,
+            }}
+          />
+          {number && (
+            <img
+              src={numberValToImg[number]}
+              alt={number}
+              style={{
+                width: "35%",
+                height: "30%",
+                position: "absolute",
+                zIndex: 2,
+              }}
+            ></img>
+          )}
+        </div>
+      ))}
+    </div>
+  );
 }
