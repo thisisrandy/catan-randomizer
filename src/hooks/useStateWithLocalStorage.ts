aa840fe1786f7ba9dbf6ac337ff57e181bdda18d
Randy West <randywest55@gmail.com>
2022-06-07 17:41:23 -0700

    Fix filter type

--- src/hooks/useStateWithLocalStorage.ts
+++ src/hooks/useStateWithLocalStorage.ts
@@ -11,7 +11,7 @@ import React, { useState, useEffect } from "react";
 export function useStateWithLocalStorage<T>(
   key: string,
   initialValue: T,
-  filter: (value: T, index: number, array: T[]) => boolean = () => true
+  filter: (value: any, index: number, array: any[]) => boolean = () => true
 ): [T, React.Dispatch<React.SetStateAction<T>>] {
   if (key in localStorage)
     initialValue = JSON.parse(localStorage.getItem(key)!);
