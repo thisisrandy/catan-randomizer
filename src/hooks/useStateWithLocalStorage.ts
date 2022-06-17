5526c53b2114e1e4fa2d40d7a0e8c6d40fcc945b
Randy West <randywest55@gmail.com>
2022-06-16 22:45:47 -0700

    Support Date type in useStateWithLocalStorage

--- src/hooks/useStateWithLocalStorage.ts
+++ src/hooks/useStateWithLocalStorage.ts
@@ -7,6 +7,12 @@ import React, { useState, useEffect } from "react";
  * every change. If `filter` is specified and `T` is an array, `filter` is
  * applied to the array before every write. Likely not performant on complex or
  * frequently-written data.
+ *
+ * Note that not all types have native JSON support. Of those, `Map` and `Date`
+ * are currently supported by this function. Behavior for any other unsupported
+ * type is to become a string or disappear entirely, e.g. as with function
+ * types, per the [ECMA
+ * spec](https://tc39.es/ecma262/multipage/structured-data.html#sec-json.stringify)
  */
 export function useStateWithLocalStorage<T>(
   key: string,
@@ -35,11 +41,29 @@ export function useStateWithLocalStorage<T>(
 // JSON.stringify/JSON.parse, respectively. stolen shamelessly from
 // https://stackoverflow.com/a/56150320/12162258
 
-function replacer(_: string, value: any) {
+// Date is also not supported, but it suffers from an additional devious trick:
+// Date.toJSON is called *before* the replacer, so any dates are already
+// stringified once they reach replace. however, according to
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/JSON/stringify#the_replacer_parameter,
+// "The object in which the key was found is provided as the replacer's this
+// parameter," so we can look up the real object there. another option is to
+// save Date.prototype.toJSON in a temp var, delete it, run JSON.stringify, and
+// then replace it from the temp var
+
+function replacer(
+  this: { [key: string]: [value: any] },
+  key: string,
+  value: any
+) {
   if (value instanceof Map) {
     return {
       dataType: "Map",
       value: [...value],
+    };
+  } else if (this[key] instanceof Date) {
+    return {
+      dataType: "Date",
+      value: value,
     };
   } else {
     return value;
@@ -47,9 +71,16 @@ function replacer(_: string, value: any) {
 }
 
 function reviver(_: string, value: any) {
-  if (typeof value === "object" && value !== null) {
-    if (value.dataType === "Map") {
-      return new Map(value.value);
+  if (
+    typeof value === "object" &&
+    value !== null &&
+    typeof value.dataType === "string"
+  ) {
+    switch (value.dataType) {
+      case "Map":
+        return new Map(value.value);
+      case "Date":
+        return new Date(value.value);
     }
   }
   return value;
