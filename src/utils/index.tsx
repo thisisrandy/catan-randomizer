fe956871dd54d9628c9370b7e8a6ff12e0eccf43
Randy West <randywest55@gmail.com>
2022-06-16 21:23:52 -0700

    Add getDaysSince util

---
+++ src/utils/index.tsx
@@ -0,0 +1,15 @@
+/**
+ * Return the number of whole days that have passed between time and now. For
+ * example, if time corresponds to 1/1/20 11:59 PM and it is now 1/2/20 12:00AM,
+ * the return value is 1, because although only 1 minute has passed, the day (in
+ * local time) has changed
+ */
+export function getDaysSince(time: Date | number, now = Date.now()) {
+  const midnight = (t: Date | number) => {
+    t = t instanceof Date ? t : new Date(t);
+    return new Date(t.toDateString()).valueOf();
+  };
+
+  // 24 * 60 * 60 * 1000 = 86400000
+  return Math.floor((midnight(now) - midnight(time)) / 86400000);
+}
