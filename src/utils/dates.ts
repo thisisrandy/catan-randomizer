b61d763ce1b09b5caa9f4d33a03d1b7ecce6461e
Randy West <randywest55@gmail.com>
2022-06-25 14:44:52 -0700

    Minor point out function params in docstring

--- src/utils/dates.ts
+++ src/utils/dates.ts
@@ -1,8 +1,8 @@
 /**
  * Return the number of whole days that have passed between time and now. For
- * example, if time corresponds to 1/1/20 11:59 PM and it is now 1/2/20 12:00AM,
- * the return value is 1, because although only 1 minute has passed, the day (in
- * local time) has changed
+ * example, if `time` corresponds to 1/1/20 11:59 PM and it is `now` 1/2/20
+ * 12:00AM, the return value is 1, because although only 1 minute has passed,
+ * the day (in local time) has changed
  */
 export function getDaysSince(time: Date | number, now = Date.now()) {
   const midnight = (t: Date | number) => {
