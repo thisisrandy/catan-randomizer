93b47e958bf05096f6d6f84f3d11e96abfd00013
Randy West <randywest55@gmail.com>
2022-07-04 22:53:38 -0700

    Minor fix mangled urls

--- src/utils/serialization.ts
+++ src/utils/serialization.ts
@@ -2,12 +2,12 @@
  * There is no native support for Map (de)serialization, but it is easily
  * supported via the replacer/reviver functions which can be passed to
  * JSON.stringify/JSON.parse, respectively. The code below is modified from
- * https:*stackoverflow.com/a/56150320/12162258.
+ * https://stackoverflow.com/a/56150320/12162258.
 
  * Date is also not supported, but it suffers from an additional devious trick:
  * Date.toJSON is called *before* the replacer, so any dates are already
  * stringified once they reach replace. However, according to
- * https:*developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/JSON/stringify#the_replacer_parameter,
+ * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/JSON/stringify#the_replacer_parameter,
  * "The object in which the key was found is provided as the replacer's this
  * parameter," so we can look up the real object there. Another option is to
  * save Date.prototype.toJSON in a temp var, delete it, run JSON.stringify, and
