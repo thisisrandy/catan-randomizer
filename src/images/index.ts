37f1660cb7d29bee1c75e160162ac6ea354e5306
Randy West <randywest55@gmail.com>
2024-02-07 19:37:02 -0800

    Organize imports/exports

--- src/images/index.ts
+++ src/images/index.ts
@@ -1,21 +1,3 @@
-import pastureHorizontal from "./pasture-horizontal.png";
-import pastureVertical from "./pasture-vertical.png";
-import forestHorizontal from "./forest-horizontal.png";
-import forestVertical from "./forest-vertical.png";
-import hillsHorizontal from "./hills-horizontal.png";
-import hillsVertical from "./hills-vertical.png";
-import mountainsHorizontal from "./mountains-horizontal.png";
-import mountainsVertical from "./mountains-vertical.png";
-import fieldsHorizontal from "./fields-horizontal.png";
-import fieldsVertical from "./fields-vertical.png";
-import desertHorizontal from "./desert-horizontal.png";
-import desertVertical from "./desert-vertical.png";
-import goldHorizonal from "./gold-horizontal.png";
-import goldVertical from "./gold-vertical.png";
-import seaHorizontal from "./sea-horizontal.png";
-import seaVertical from "./sea-vertical.png";
-import fogHorizontal from "./fog-horizontal.png";
-import fogVertical from "./fog-vertical.png";
 import two from "./2.png";
 import three from "./3.png";
 import four from "./4.png";
@@ -32,22 +14,38 @@ import portGrainWithDocks from "./port-grain-with-docks.png";
 import portOreWithDocks from "./port-ore-with-docks.png";
 import portTimberWithDocks from "./port-timber-with-docks.png";
 import portWoolWithDocks from "./port-wool-with-docks.png";
-import castleKnightsHorizontal from "castle-knights-horizontal.png";
-import castleKnightsVertical from "castle-knights-vertical.png";
-import castleRestorationHorizontal from "castle-restoration-horizontal.png";
-import castleRestorationVertical from "castle-restoration-vertical.png";
-import fish10 from "fish-10.png";
 import fish4 from "fish-4.png";
 import fish5 from "fish-5.png";
 import fish6 from "fish-6.png";
 import fish8 from "fish-8.png";
 import fish9 from "fish-9.png";
+import fish10 from "fish-10.png";
+import castleKnightsHorizontal from "castle-knights-horizontal.png";
+import castleKnightsVertical from "castle-knights-vertical.png";
+import castleRestorationHorizontal from "castle-restoration-horizontal.png";
+import castleRestorationVertical from "castle-restoration-vertical.png";
+import desertHorizontal from "./desert-horizontal.png";
+import desertVertical from "./desert-vertical.png";
+import fieldsHorizontal from "./fields-horizontal.png";
+import fieldsVertical from "./fields-vertical.png";
+import fogHorizontal from "./fog-horizontal.png";
+import fogVertical from "./fog-vertical.png";
+import forestHorizontal from "./forest-horizontal.png";
+import forestVertical from "./forest-vertical.png";
 import glasswordsHorizontal from "glasswords-horizontal.png";
 import glasswordsVertical from "glasswords-vertical.png";
+import goldHorizonal from "./gold-horizontal.png";
+import goldVertical from "./gold-vertical.png";
+import hillsHorizontal from "./hills-horizontal.png";
+import hillsVertical from "./hills-vertical.png";
 import lakeHorizontal from "lake-horizontal.png";
 import lakeVertical from "lake-vertical.png";
+import mountainsHorizontal from "./mountains-horizontal.png";
+import mountainsVertical from "./mountains-vertical.png";
 import oasisHorizontal from "oasis-horizontal.png";
 import oasisVertical from "oasis-vertical.png";
+import pastureHorizontal from "./pasture-horizontal.png";
+import pastureVertical from "./pasture-vertical.png";
 import quarryHorizontal from "quarry-horizontal.png";
 import quarryVertical from "quarry-vertical.png";
 import riverHillsHorizontal from "river-hills-horizontal.png";
@@ -60,26 +58,10 @@ import riverSwamplandBottomHorizontal from "river-swampland-bottom-horizontal.pn
 import riverSwamplandBottomVertical from "river-swampland-bottom-vertical.png";
 import riverSwamplandTopHorizontal from "river-swampland-top-horizontal.png";
 import riverSwamplandTopVertical from "river-swampland-top-vertical.png";
+import seaHorizontal from "./sea-horizontal.png";
+import seaVertical from "./sea-vertical.png";
 
 export {
-  pastureHorizontal,
-  pastureVertical,
-  forestHorizontal,
-  forestVertical,
-  hillsHorizontal,
-  hillsVertical,
-  mountainsHorizontal,
-  mountainsVertical,
-  fieldsHorizontal,
-  fieldsVertical,
-  desertHorizontal,
-  desertVertical,
-  goldHorizonal,
-  goldVertical,
-  seaHorizontal,
-  seaVertical,
-  fogHorizontal,
-  fogVertical,
   two,
   three,
   four,
@@ -96,22 +78,38 @@ export {
   portOreWithDocks,
   portTimberWithDocks,
   portWoolWithDocks,
-  castleKnightsHorizontal,
-  castleKnightsVertical,
-  castleRestorationHorizontal,
-  castleRestorationVertical,
-  fish10,
   fish4,
   fish5,
   fish6,
   fish8,
   fish9,
+  fish10,
+  castleKnightsHorizontal,
+  castleKnightsVertical,
+  castleRestorationHorizontal,
+  castleRestorationVertical,
+  desertHorizontal,
+  desertVertical,
+  fieldsHorizontal,
+  fieldsVertical,
+  fogHorizontal,
+  fogVertical,
+  forestHorizontal,
+  forestVertical,
   glasswordsHorizontal,
   glasswordsVertical,
+  goldHorizonal,
+  goldVertical,
+  hillsHorizontal,
+  hillsVertical,
   lakeHorizontal,
   lakeVertical,
+  mountainsHorizontal,
+  mountainsVertical,
   oasisHorizontal,
   oasisVertical,
+  pastureHorizontal,
+  pastureVertical,
   quarryHorizontal,
   quarryVertical,
   riverHillsHorizontal,
@@ -124,4 +122,6 @@ export {
   riverSwamplandBottomVertical,
   riverSwamplandTopHorizontal,
   riverSwamplandTopVertical,
+  seaHorizontal,
+  seaVertical,
 };
