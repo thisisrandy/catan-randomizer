a019e8f40f1315293b0dbb4376474e7b40c4d9dd
Randy West <randywest55@gmail.com>
2024-02-07 19:30:49 -0800

    Add T&B images to index

--- src/images/index.ts
+++ src/images/index.ts
@@ -32,6 +32,34 @@ import portGrainWithDocks from "./port-grain-with-docks.png";
 import portOreWithDocks from "./port-ore-with-docks.png";
 import portTimberWithDocks from "./port-timber-with-docks.png";
 import portWoolWithDocks from "./port-wool-with-docks.png";
+import castleKnightsHorizontal from "castle-knights-horizontal.png";
+import castleKnightsVertical from "castle-knights-vertical.png";
+import castleRestorationHorizontal from "castle-restoration-horizontal.png";
+import castleRestorationVertical from "castle-restoration-vertical.png";
+import fish10 from "fish-10.png";
+import fish4 from "fish-4.png";
+import fish5 from "fish-5.png";
+import fish6 from "fish-6.png";
+import fish8 from "fish-8.png";
+import fish9 from "fish-9.png";
+import glasswordsHorizontal from "glasswords-horizontal.png";
+import glasswordsVertical from "glasswords-vertical.png";
+import lakeHorizontal from "lake-horizontal.png";
+import lakeVertical from "lake-vertical.png";
+import oasisHorizontal from "oasis-horizontal.png";
+import oasisVertical from "oasis-vertical.png";
+import quarryHorizontal from "quarry-horizontal.png";
+import quarryVertical from "quarry-vertical.png";
+import riverHillsHorizontal from "river-hills-horizontal.png";
+import riverHillsVertical from "river-hills-vertical.png";
+import riverMountainsHorizontal from "river-mountains-horizontal.png";
+import riverMountainsVertical from "river-mountains-vertical.png";
+import riverPastureHorizontal from "river-pasture-horizontal.png";
+import riverPastureVertical from "river-pasture-vertical.png";
+import riverSwamplandBottomHorizontal from "river-swampland-bottom-horizontal.png";
+import riverSwamplandBottomVertical from "river-swampland-bottom-vertical.png";
+import riverSwamplandTopHorizontal from "river-swampland-top-horizontal.png";
+import riverSwamplandTopVertical from "river-swampland-top-vertical.png";
 
 export {
   pastureHorizontal,
@@ -68,4 +96,32 @@ export {
   portOreWithDocks,
   portTimberWithDocks,
   portWoolWithDocks,
+  castleKnightsHorizontal,
+  castleKnightsVertical,
+  castleRestorationHorizontal,
+  castleRestorationVertical,
+  fish10,
+  fish4,
+  fish5,
+  fish6,
+  fish8,
+  fish9,
+  glasswordsHorizontal,
+  glasswordsVertical,
+  lakeHorizontal,
+  lakeVertical,
+  oasisHorizontal,
+  oasisVertical,
+  quarryHorizontal,
+  quarryVertical,
+  riverHillsHorizontal,
+  riverHillsVertical,
+  riverMountainsHorizontal,
+  riverMountainsVertical,
+  riverPastureHorizontal,
+  riverPastureVertical,
+  riverSwamplandBottomHorizontal,
+  riverSwamplandBottomVertical,
+  riverSwamplandTopHorizontal,
+  riverSwamplandTopVertical,
 };
